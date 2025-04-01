51d4de9d63bcd15ce2f6392ee1318e4cf91a06cd
Kazuyoo_12 <dikinurrohmat42@gmail.com>
2025-04-01 13:43:46 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -18,27 +18,10 @@ API=$(getprop ro.build.version.sdk)
 
 # trimming
 trim_partition () {
-    fstrim -v /system
-    sleep 0.1
-    fstrim -v /vendor
-    sleep 0.1
-    fstrim -v /data
-    sleep 0.1
-    fstrim -v /cache
-    sleep 0.1
-    fstrim -v /metadata
-    sleep 0.1
-    fstrim -v /odm
-    sleep 0.1
-    fstrim -v /system_ext
-    sleep 0.1
-    fstrim -v /product
-}
-
-cleanup_gpu() {
-for i in "$(find /data -type f -name '*shader*')"; do
-    rm -f $i
-done
+    for partition in system vendor data cache metadata odm system_ext product; do
+        fstrim -v "/$partition"
+        sleep 0.1
+    done
 }
 
 # delete trash & log by @Bias_khaliq
@@ -53,17 +36,17 @@ for DIR in /data/data/*; do
   fi
 done
 
-# Delete Logs
-rm -rf /data/{anr,log,tombstones,log_other_mode}/* \
-       /cache/*.{apk,tmp} \
-       /dev/log/* \
-       /data/system/{dropbox,usagestats,package_cache}/* \
-       /sys/kernel/debug/* \
-       /data/local/tmp* \
-       /data/dalvik-cache \
-       /data/media/0/{DCIM,Pictures,Music,Movies}/.thumbnails \
-       /data/media/0/{mtklog,MIUI/Gallery,MIUI/.debug_log,MIUI/BugReportCache} \
-       /data/vendor/thermal/{config,*.dump,*_history*.dump}
+# Delete cache
+find /data/{anr,log,tombstones,log_other_mode}/* -delete
+find /cache/*.{apk,tmp} -delete
+find /dev/log/* -delete
+find /sys/kernel/debug/* -delete
+find /data/local/tmp/* -delete
+find /data/dalvik-cache/* -delete
+find /data/media/0/{DCIM,Pictures,Music,Movies}/.thumbnails -delete
+find /data/media/0/{mtklog,MIUI/Gallery,MIUI/.debug_log,MIUI/BugReportCache} -delete
+find /data/vendor/thermal/{config,*.dump,*_history*.dump} -delete
+find /sdcard/Android/data/*/cache -delete
 }
 
 sleep 0.2
@@ -101,8 +84,6 @@ sleep 0.5
 ui_print "- Delete trash and logs"
 delete_trash_logs
 sleep 0.5
-ui_print "- Clean Gpu Shader"
-cleanup_gpu &>/dev/null
 
 # Set permissions
 set_perm_recursive $MODPATH 0 0 0755 0644
